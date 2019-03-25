7580e41455fc165fc5152409e9e7a1f0792699b6
Tsukune83t <nico83t@gmail.com>
2019-03-25 12:03:21 +0100

    mmm

---
+++ src/Travel.js
@@ -0,0 +1,19 @@
+// src/Travel.js
+import React from "react";
+
+const Travel = () => (
+  <figure>
+    <img
+      src=""
+      alt="Photo"
+    />
+    <figcaption>
+      <blockTravel>
+        Shoplifting is a victimless crime, like punching someone in the dark.
+      </blockTravel>
+      <cite></cite>
+    </figcaption>
+  </figure>
+);
+
+export default Travel;
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+/ src/Travel.js
+import React from "react";
+
+const Travel = () => (
+  <figure>
+    
+    <figcaption>
+      <blocktravel>
+       
+      </blocktravel>
+      <cite></cite>
+    </figcaption>
+  </figure>
+);
+
+export default Travel
